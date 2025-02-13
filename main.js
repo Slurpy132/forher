1e4fe28489ce117e620ccfd467fac3f02d0a26ea
Slurpy <75202702+Slurpy132@users.noreply.github.com>
2025-02-13 19:48:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function goFirst(){
+    window.location.href="page_yes.html";
+}
+function goSecond(){
+    window.location.href="page_no.html";
+}
+function goBack(){
+    window.location.href="ok.html";
+}
+function playWanna(){
+    myAudio.play();
+}
+function playHere(){
+    MYaudio.play();
+}
+function playCreep(){
+    myaudio.play();
+}
